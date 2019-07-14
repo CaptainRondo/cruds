e102fa3382e5635de67c25eb2dab7ecf51b00382
CaptainRondo <41194085+CaptainRondo@users.noreply.github.com>
2019-07-14 15:55:36 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# TODO
+# make sure this is running as root
+
 cd /tmp
 echo "Check if git is installed..."
 git --version
+
+# TODO
+# switch to Beowulf repos
+# apt install xinit xserver-xorg
